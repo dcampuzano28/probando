23d0203f6355e4465ede0784c393a35023ec1e0a
johandork <johanw.sanchez@correo.usa.edu.co>
2020-03-16 23:17:32 -0500

    Se termino la vista registro con ngmodel y evento boton

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
+  nombres:String
+  apellidos:String
+  usuario:String
+  correoelectronico:String
+  carrera:String
+  genero:String
+  contrasena:String
+  recontrasena:String
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  validarRegistro(){
+    console.log("Se registro: "+this.nombres)
+  }
+
 
 }
