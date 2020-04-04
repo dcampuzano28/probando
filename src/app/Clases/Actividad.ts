ead28c6bbe75468acacf379558d67bf60467a870
Daniel Campuzano R <daniel.campuzano@correo.usa.edu.co>
2020-04-04 17:03:58 +0000

    Update Actividad.ts

--- src/app/Clases/Actividad.ts
+++ src/app/Clases/Actividad.ts
@@ -2,10 +2,19 @@ export interface Actividad{
     id_actividad:number 
     HoraIn:number
     HoraFn:number
-    NombreA:String
-    DescripcionA:String
+    nom_actividad:String
+    des_actividad:String
     Motivacion:String
-    prioridad:number
-    Dia:number 
- 
+    id_prioridad:number
+    dia_actividad:String 
+
+ /* "id_actividad": 1,
+      "hora_actividad": "18",
+      "dia_actividad": "lunes",
+      "nom_actividad" : "Prueba",
+      "des_actividad": "descripcion",
+      "estado_actividad": false,
+      "id_prioridad" :1,
+      "id_estudiante":2*/
+
 }
